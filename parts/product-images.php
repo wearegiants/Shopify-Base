2b1e3b16e02f5bec5eb1908f7dc0b959e81e3da3
Craig Fowler <craig@wearegiants.com>
2015-07-24 20:52:32 -0700

    junk

--- parts/product-images.php
+++ parts/product-images.php
@@ -1,4 +1,4 @@
-<div id="product--images" class="carousel" data-carousel-options='{"maxWidth":"740px", "pagination":false}'>
+<div id="product--images" class="carousel" data-carousel-options='{"maxWidth":"960px", "pagination":false}'>
    <div class="slide"><img src="/assets/NOON-GOONS-05.jpg" class="img-responsive" alt="" /></div>
    <div class="slide"><img src="/assets/NOON-GOONS-06.jpg" class="img-responsive" alt="" /></div>
 </div>
