786f8925cfde14c43b64ec086eb57e6f9e0c745b
Craig Fowler <craig@wearegiants.com>
2015-11-10 11:18:14 -0800

    updated footer

--- parts/footer.php
+++ parts/footer.php
@@ -1,17 +1,15 @@
 <footer id="footer">
   <div class="fs-row">
-    <menu id="footer--nav" class="fs-cell fs-xl-3 fs-lg-3 fs-md-3 fs-sm-3">
+    <menu class="fs-cell fs-all-half fs-sm-full">
+      <span class="btn btn--nav">Time Line 2015</span>
       <a href="?page=general" class="btn btn--nav">Contact Us</a>
       <a href="?page=general" class="btn btn--nav">Privacy Policy</a>
     </menu>
-    <menu id="footer--copyright" class="fs-cell fs-xl-6 fs-lg-6 fs-md-3 fs-sm-3 text-ceter">
-      <span class="btn btn--nav">Time Line 2015</span>
-    </menu>
-    <menu id="footer--social" class="fs-cell fs-xl-3 fs-lg-3 fs-md-3 fs-sm-3 text-right">
+    <menu id="footer--social" class="fs-cell fs-all-half fs-sm-full">
       <a href="#" class="btn btn--nav btn--nav_icon ss-social ss-facebook"  title="facebook"></a>
       <a href="#" class="btn btn--nav btn--nav_icon ss-social ss-twitter"   title="twitter"></a>
       <a href="#" class="btn btn--nav btn--nav_icon ss-social ss-instagram" title="instagram"></a>
-      <a href="#" class="btn btn--nav btn--nav_icon ss-social ss-pinterest" title="facebook"></a>
+      <a href="#" class="btn btn--nav btn--nav_icon ss-social ss-pinterest" title="pinterest"></a>
     </menu>
   </div>
 </footer>
